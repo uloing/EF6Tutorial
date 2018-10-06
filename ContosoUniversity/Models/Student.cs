2b9165a26b99a61b8ff5376d4f30bef18db2d5eb
Sven Olding <sven.olding@gmx.de>
2018-10-06 17:01:52 +0200

    add date datatype with display format to enrollment date

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
@@ -9,6 +10,8 @@ namespace ContosoUniversity.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
         public string Secret { get; set; }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
